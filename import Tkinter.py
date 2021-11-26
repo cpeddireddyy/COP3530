7f1a34100a349d673c79747a7d6cae4a9d45efe0
evanandresen <evanandresen@ufl.edu>
2021-11-25 23:50:03 -0500

    GUI update 1/2 screen
    Figured out how to use tkinter and got left half of screen done.  Also included launch.json file for debug configing. "integratedTerminal" vs "externalTerminal"

--- import Tkinter.py
+++ import Tkinter.py
@@ -1,4 +1,39 @@
-import Tkinter
-top = Tkinter.Tk()
+from tkinter import *
+
+
+def startNav():
+    #shortest path s-t
+    #redisplay path
+    print("starting nav")
+
+def step1():
+    print("step 1")
+def step10():
+    print("step 10")
+def step100():
+    print("step 100")
+def step1000():
+    print("step 1000")
+
+
+gui = Tk(className = "iMaps - COP3530 Graph Project\tEvan & Chaitra")
 # Code to add widgets will go here...
-top.mainloop()
+descript_label = Label(gui, text = "Welcome to iMaps created by Evan & Chaitra!\nThis navigation app uses a road network of\nthe United States and gives the fastest route\nfrom any two points. Enter a location\n(number 1-129163) to start and end navigation\nthen click \"Start Navigation\". Step through\neach direction using the buttons below.", bg="#e33939", font=("Courier", 16), width = 46, height = 9)
+descript_label.place(x=20, y=20)
+my_label = Label(gui, text = "Step through navigation\n (step size)", bg="#e33939", font=("Courier", 26), width = 28, height = 2)
+my_label.place(x=20, y=460)
+gui.geometry("1280x720")
+startNav_button = Button(gui, command = startNav, text = "Start Navigation",font=("Courier", 26), bg="#61aaed", width = 28, height = 2)
+startNav_button.place(x=20, y=340)
+step1_button = Button(gui, command = step1, text = "  1 step  ",font=("Courier", 20), bg="#61aaed", width = 7, height = 2)
+step1_button.place(x=20, y=560)
+step10_button = Button(gui, command = step10, text = " 10 step ",font=("Courier", 20), bg="#61aaed", width = 8, height = 2)
+step10_button.place(x=150, y=560)
+step100_button = Button(gui, command = step100, text = " 100 step ",font=("Courier", 20), bg="#61aaed", width = 9, height = 2)
+step100_button.place(x=295, y=560)
+step1000_button = Button(gui, command = step1000, text = "1000 step",font=("Courier", 20), bg="#61aaed", width = 9, height = 2)
+step1000_button.place(x=455, y=560)
+gui.mainloop()
+
+
+
